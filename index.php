ba0979e34301a0ecbf23d17d4c8d5d1e1d5dd3e8
gsgodoy <gustavosouzagodoy@hotmail.com>
2021-02-04 14:11:34 -0300

    colocando quadrado

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 <h3> Um deploy aotomático feito com alunos do itaú </h3>
 
-<h5>Tentar sempre, desistir jamais!!!<h5>
+<h5>Tentar sempre, desistir jamais!!!</h5>
   
-<h7>testando!!!<h7>
-<h10>Tentar sempre, desistir as vezes!!!<h10>
-<h7>QUERO DELETAR TUDO </h7>
+<h5>testando!!!</h5>
+<h5>Tentar sempre, desistir as vezes!!!</h5>
+<h5>QUERO DELETAR TUDO </h5>
+
+
+<div style="width:200px;height:200px;background:yellow">
+    Um teste em um quadrado
+</div>
