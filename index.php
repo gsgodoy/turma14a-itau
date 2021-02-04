e9f7a8bb888bd17c35fa9985e7c37e2cc95c0b17
gsgodoy <gustavosouzagodoy@hotmail.com>
2021-02-04 16:20:55 -0300

    botoes

--- index.php
+++ index.php
@@ -3,15 +3,27 @@
 <h3> Um deploy automático feito com alunos do itaú </h3>
 
 <div style="display:flex">
-  <div style="width:200px;height:200px;background:yellow;color:blue;padding:20px;margin:10px">
+  <div style="width:200px;height:350px;background:yellow;color:blue;padding:20px;margin:10px">
       <b>Amarelo</b>
       <br>
       <img src="fig1.jpg" style="width: 200px">
+      <br><br>
+      <b>R$ 500,00</b>
+      <br><br><br>
+      <a href="url pgseguro" target="_blank" style="display:bloc;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Experimente</a><br>
+      <br><br>
+      <a href="url whatsapp" target="_blank">Entrar em contato</a><br>
   </div>
 
-  <div style="width:200px;height:200px;background:blue;color:yellow;padding:20px;margin:10px">
+  <div style="width:200px;height:350px;background:red;color:yellow;padding:20px;margin:10px">
       <b>Azul</b>
       <br>
       <img src="fig2.jfif" style="width: 200px">
+      <br><br>
+      <b>R$ 500,00</b>
+      <br><br><br>
+      <a href="url pgseguro" target="_blank" style="display:bloc;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Experimente</a><br>
+      <br><br>
+      <a href="url whatsapp" target="_blank">Entrar em contato</a><br>
   </div>
 </div>
