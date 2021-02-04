bdfb595ada5b74291dc43ad27011f44f6d1e86d4
gsgodoy <gustavosouzagodoy@hotmail.com>
2021-02-04 15:14:21 -0300

    figuras

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
 <div style="display:flex">
   <div style="width:200px;height:200px;background:yellow;color:blue;padding:20px;margin:10px">
       <b>Amarelo</b>
+      <br>
+      <img src="fig1.jpg" style="width: 200px">
   </div>
 
   <div style="width:200px;height:200px;background:blue;color:yellow;padding:20px;margin:10px">
       <b>Azul</b>
+      <br>
+      <img src="fig2.jfif" style="width: 200px">
   </div>
 </div>
